47d95619b2e5948fc10cd1b684dbc3c103f2b904
Hema Prathaban <hema.prathaban@puri.sm>
2017-06-05 02:37:33 -0400

    Generating libraries and headers file for puremail
    Signed-off-by: Hema Prathaban <hema.prathaban@puri.sm>

--- debian/tests/idlTest.sh
+++ debian/tests/idlTest.sh
@@ -15,22 +15,22 @@ TESTFILE=$(basename $0 .sh)
 echo "Running tests in ${TESTFILE}"
 
 echo -n "Test1: Make sure we can generate typelibs..."
-/usr/lib/thunderbird-devel/sdk/bin/typelib.py \
-    -I"/usr/lib/thunderbird-devel/idl" \
+/usr/lib/puremail-devel/sdk/bin/typelib.py \
+    -I"/usr/lib/puremail-devel/idl" \
     -o "${SCRATCHDIR}/test.xpt" \
     "${TEST_IDL_FILE}"
 echo "done."
 
 echo -n "Test2: Make sure we can generate C++ headers..."
-/usr/lib/thunderbird-devel/sdk/bin/header.py \
-    -I"/usr/lib/thunderbird-devel/idl" \
+/usr/lib/puremail-devel/sdk/bin/header.py \
+    -I"/usr/lib/puremail-devel/idl" \
     -o "${SCRATCHDIR}/test.h" \
     "${TEST_IDL_FILE}"
 echo "done."
 
 echo -n "Test3: Compiling generated file..."
 g++ -std=c++11 \
-    -I/usr/include/thunderbird \
+    -I/usr/include/puremail \
     -I/usr/include/nspr    \
     -o "${SCRATCHDIR}/test.o" \
     "${SCRATCHDIR}/test.h"
